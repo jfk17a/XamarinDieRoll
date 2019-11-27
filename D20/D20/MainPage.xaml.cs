fcf6796c949be421d068b3ab1d7924a863a58ea7
jfk17a <44479985+jfk17a@users.noreply.github.com>
2019-11-26 22:17:32 -0600

    Add project files.

---
+++ D20/D20/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace D20
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public Random rnd = new Random();
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void DieRoll(object sender, EventArgs e)
+        {
+            DieValue.Text = rnd.Next(1,20).ToString();
+        }
+    }
+}
